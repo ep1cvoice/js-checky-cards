1ec430c0a76b7d2f7990fc8daf222b1614697d99
Pavel Kovalchuk <pavelkovalchuk0510@gmail.com>
2026-02-16 11:43:15 +0100

    useEffect for cards, move db api url

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,48 +1,31 @@
+import { useState, useEffect } from 'react';
+import { API_URL } from '../../constants';
 import QuestionCard from '../../components/QuestionCard';
-
+import Button from '../../components/Button';
 import styles from './HomePage.module.css';
 
-const cards = [
-	{
-		id: '1',
-		question: 'Что такое React?',
-		answer: 'React — это библиотека для создания пользовательских интерфейсов.',
-		description:
-			'React — это JavaScript-библиотека, разработанная Facebook, которая используется для построения UI с компонентным подходом. React позволяет вам создавать пользовательские интерфейсы из отдельных частей, называемых компонентами.',
-		resources: ['https://react.dev', 'https://react.dev/reference/react'],
-		level: 1,
-		completed: true,
-		editDate: '03.02.2025, 19:49',
-	},
-	{
-		id: '2',
-		question: 'Czym jest JSX?',
-		answer: 'JSX to rozszerzenie składni JavaScript dla Reacta.',
-		description:
-			'JSX pozwala pisać kod podobny do HTML w JavaScript, który następnie jest transpilowany do wywołań `React.createElement`. Ułatwia to tworzenie i wizualne przedstawienie struktury komponentów.',
-		resources: ['https://react.dev/learn/writing-markup-with-jsx'],
-		level: 2,
-		completed: false,
-		editDate: '03.02.2025, 20:25',
-	},
-	{
-		id: '3',
-		question: 'Jaka jest główna zasada działania Reacta?',
-		answer: 'React wykorzystuje Virtual DOM do optymalizacji renderowania.',
-		description:
-			'React opiera się na koncepcji Virtual DOM — jest to reprezentacja rzeczywistego DOM w pamięci. Gdy stan komponentu się zmienia, React porównuje Virtual DOM z jego poprzednim stanem i aktualizuje na stronie tylko zmienione elementy.',
-		resources: ['https://react.dev/learn/render-and-commit'],
-		level: 2,
-		completed: false,
-		editDate: '03.02.2025, 19:01',
-	},
-];
-
 const HomePage = () => {
+	const [questions, setQuestions] = useState([]);
+
+	const getQuestions = async () => {
+		try {
+			const response = await fetch(`${API_URL}/react`);
+			const data = await response.json();
+
+			setQuestions(data);
+		} catch (error) {
+			console.error(error);
+		}
+	};
+
+	useEffect(() => {
+		getQuestions();
+	}, []);
+
 	return (
 		<>
-			{cards.map((card, index) => {
-				return <QuestionCard card={card} key={card.id} />;
+			{questions.map((card, index) => {
+				return <QuestionCard card={card} key={index} />;
 			})}
 		</>
 	);
